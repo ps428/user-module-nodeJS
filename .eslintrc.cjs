1e6b88f24bc03430c9d0172ef35bbbf413c1b3f8
Pranav <pranav.bhawan@gmail.com>
2022-06-28 16:40:29 +0530

    ESLINT CONFIG ADDED

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+};
