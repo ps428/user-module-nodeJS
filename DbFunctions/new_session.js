33efadab3ae70335e3897642dc68cfcfc5292e87
Pranav <pranav.bhawan@gmail.com>
2022-06-28 16:13:11 +0530

    Es lint executed

--- DbFunctions/new_session.js
+++ DbFunctions/new_session.js
@@ -1,143 +1,128 @@
-import crypto from "crypto";
-import DbOperation from "db_pkg";
+import crypto from 'crypto';
+import DbOperation from 'db_pkg';
 
-const generate_session_key = async function(){
-    return crypto.randomBytes(8).toString("base64");
+const generateSessionKey = async function() {
+  return crypto.randomBytes(8).toString('base64');
 };
 
-export default class SessionClass{
-    constructor(){
+export default class SessionClass {
+  constructor() {
 
-    }
+  }
 
-async  insertSession(username,refreshtoken){
+  async insertSession(username, refreshtoken) {
     let result;
 
     const u_name = username;
     const ref_token = refreshtoken;
 
-    const generate_session_id = await generate_session_key().then(result);
-    
+    const generate_session_id = await generateSessionKey().then(result);
+
     let get_user_id;
     let user_id;
-    let fetch_condition = [u_name];
+    const fetch_condition = [u_name];
 
-    try{
-        const get_sql = "select id from users where email=?";
-        
-        get_user_id = await DbOperation.execCustomQuery(get_sql,fetch_condition);
-        
-        user_id = get_user_id[0].id;
+    try {
+      const get_sql = 'select id from users where email=?';
 
-        }
+      get_user_id = await DbOperation.execCustomQuery(get_sql, fetch_condition);
 
-    catch(error){
+      user_id = get_user_id[0].id;
+    } catch (error) {
       //  console.log(error);
-        result = {success:false,msg:error};
-        return result;
+      result = {success: false, msg: error};
+      return result;
     }
 
     let currentdate;
-    currentdate = new Date().toISOString().slice(0, 19).replace("T", " ");
-   
-    let insert_table = "sessions"; 
-    let flag_insert_success = 0 ;
-    
-    let fieldsData = [
-        "session_id",
-        "user_id",
-        "login_time",
-        "token"
+    currentdate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+    const insert_table = 'sessions';
+    let flag_insert_success = 0;
+
+    const fieldsData = [
+      'session_id',
+      'user_id',
+      'login_time',
+      'token',
     ];
-    let valuesData = [
-            [
-            generate_session_id,
-            user_id,
-            currentdate,
-            ref_token                       
-            ]
+    const valuesData = [
+      [
+        generate_session_id,
+        user_id,
+        currentdate,
+        ref_token,
+      ],
     ];
 
 
-    try{
-        await DbOperation.insertData("sessions",fieldsData,valuesData);
-        flag_insert_success = 1;
-        result =  {success:true,msg:"session inserted"};
-    }
-    catch(error){
-        result =  {success:false,msg:"session insertion failed"};
+    try {
+      await DbOperation.insertData('sessions', fieldsData, valuesData);
+      flag_insert_success = 1;
+      result = {success: true, msg: 'session inserted'};
+    } catch (error) {
+      result = {success: false, msg: 'session insertion failed'};
     }
 
     let get_max_id;
     let get_max_id_from_session;
     let returned_max_user_id;
 
-    let fetch_condition1 = [user_id];
+    const fetch_condition1 = [user_id];
 
-    try{
-        get_max_id = "select max(id) as maxid from sessions where user_id = ?";
-        get_max_id_from_session = await DbOperation.execCustomQuery(get_max_id,fetch_condition1);
-        returned_max_user_id = get_max_id_from_session[0].maxid;
- 
+    try {
+      get_max_id = 'select max(id) as maxid from sessions where user_id = ?';
+      get_max_id_from_session = await DbOperation.execCustomQuery(get_max_id, fetch_condition1);
+      returned_max_user_id = get_max_id_from_session[0].maxid;
+    } catch (error) {
+      console.log(error);
     }
 
-    catch(error){
-        console.log(error);
-    }
-
-    let updateData = 
-        {   
-        "logout_time": currentdate                           
+    const updateData =
+        {
+          'logout_time': currentdate,
         };
-    
-    let condData = 
-        {   
-            "user_id": user_id,
-            "id": returned_max_user_id-1
+
+    const condData =
+        {
+          'user_id': user_id,
+          'id': returned_max_user_id-1,
         };
 
 
-
-    if(flag_insert_success == 1){
-        try{
-            await DbOperation.updateData("sessions",updateData,condData);
-            result = {
-                success: true,
-                message:"previous session ended."
-               };
-        }
-        catch(error){
-            console.log("updating error"+error);
-            result = {
-                success: false,
-                message:"previous session update failed."
-               };
-        }
-
+    if (flag_insert_success == 1) {
+      try {
+        await DbOperation.updateData('sessions', updateData, condData);
+        result = {
+          success: true,
+          message: 'previous session ended.',
+        };
+      } catch (error) {
+        console.log('updating error'+error);
+        result = {
+          success: false,
+          message: 'previous session update failed.',
+        };
+      }
     }
     return result;
-}
+  }
 
 
-async  fetchSessiondata(username){
-
+  async fetchSessiondata(username) {
     let result;
     let get_session_data;
-    let fetch_condition2= [];
- 
+    const fetch_condition2= [];
+
     const query = `
     select session_id, token from sessions where id = (select max(id) from sessions) and logout_time is null;`;
 
-        try{
-            
-            get_session_data = await DbOperation.execCustomQuery(query,fetch_condition2);
-            result =  {success:true,msg:"query passed",output:get_session_data};
-        }
-
-        catch(error){
-            result = {success:false,msg:"query failed",output:get_session_data};
-        }
+    try {
+      get_session_data = await DbOperation.execCustomQuery(query, fetch_condition2);
+      result = {success: true, msg: 'query passed', output: get_session_data};
+    } catch (error) {
+      result = {success: false, msg: 'query failed', output: get_session_data};
+    }
     return result;
-
-}
+  }
 }
--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-import { loginRouter } from "./Routes/Userloginroutes.js";
+import {loginRouter} from './Routes/Userloginroutes.js';
 
-import express from "express";
-import bodyParser from "body-parser";
-import { config } from "dotenv";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import {config} from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import {userRouter} from './Routes/userRoutes.js';
 
 config();
 
 const PORT = process.env.API_PORT || 5000;
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const app = express();
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(cookieParser());
 
-// create server 
+// create server
 app.listen(PORT, () => {
-    console.log("Server is up and running on localhost:"+ PORT);
+  console.log('Server is up and running on localhost:'+ PORT);
 });
 
-app.use("/api/userlogin", loginRouter);
+app.use('/api/userlogin', loginRouter);
 app.use('/api/users', userRouter);
 
