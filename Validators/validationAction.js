b0ab9d8e1f8397976de849206111350cb94251ca
Pranav <pranav.bhawan@gmail.com>
2022-06-29 18:08:50 +0530

    Debugged no password issue

--- Validators/validationAction.js
+++ Validators/validationAction.js
@@ -1,9 +1,12 @@
+/* eslint-disable max-len */
 import {validationResult} from 'express-validator';
 
 const validatorAction = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(409).json({success: false, errors: errors.array()});
+    if (!(errors.array().length==1 && errors.array()[0].param=='password' && req.body.password=='')) {
+      return res.status(409).json({success: false, errors: errors.array()});
+    }
   }
 
   next();
