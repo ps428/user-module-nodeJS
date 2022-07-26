514e11c4f5b2d6d1386d4f25a900c93c94a468dc
bharat chitara <bharatchitara99@gmail.com>
2022-07-26 11:52:36 +0530

    cosmetic changes

--- Views/home.js
+++ Views/home.js
@@ -1,33 +1,29 @@
+/* eslint-disable require-jsdoc */
 // eslint-disable-next-line no-unused-vars
-import cookieParser from "cookie-parser";
-import { checkAccessToken } from "../Middleware/accessTokenCheck.js";
+import cookieParser from 'cookie-parser';
+import {checkAccessToken} from '../Middleware/accessTokenCheck.js';
 
 
-export function home(req,res){
-    
-    let message;
-    try{
-        checkAccessToken;
+export function home(req, res) {
+  let message;
+  try {
+    checkAccessToken;
 
-        message = {
-            
-                success: true,
-                message: "welcome home"
-        };
+    message = {
 
-        res.status(200).json(message);
-        
+      success: true,
+      message: 'welcome home',
+    };
 
-    }
-    catch(error){
-        message = {
-         
-            success: false,
-            message: error
-            
-        };
-        res.status(401).json(message);
-         
-    }
+    res.status(200).json(message);
+  } catch (error) {
+    message = {
+
+      success: false,
+      message: error,
+
+    };
+    res.status(401).json(message);
+  }
 }
 
